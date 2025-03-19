c6a4225d477b7a0b84a2b652ac64ad98ff5edb4e
0Jahid <iamjhashik@hotmail.com>
2025-03-20 02:14:09 +0600

    removed some boiler code

---
+++ app/src/main/java/com/jahid/shoppingapp/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.jahid.shoppingapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.jahid.shoppingapp.ui.theme.ShoppingAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ShoppingAppTheme {
+
+            }
+        }
+    }
+}
+
